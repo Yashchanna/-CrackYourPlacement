c37086f3fc3f8a37c63de1c117430e186865b6a9
Yashchanna <118649332+Yashchanna@users.noreply.github.com>
2025-02-05 02:39:55 +0530

    Time: 2 ms (37.63%), Space: 95.9 MB (27.52%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -10,51 +10,53 @@ public:
 
         while(i<m && j<n)
         {
-            if(nums1[i]<=nums2[j])
+            if(nums1[i]<nums2[j])
             {
-                merge.push_back(nums1[i]);
-                i++;
+                merge.push_back(nums1[i++]);
+                
             }
             else if(nums2[j]<nums1[i])
             {
-                merge.push_back(nums2[j]);
-                j++;
+                merge.push_back(nums2[j++]);
+            }
+            else if(nums1[i]==nums2[j])
+            {
+                merge.push_back(nums2[j++]);
+                merge.push_back(nums1[i++]);
             }
         }
 
         while(i<m)
         {
-             merge.push_back(nums1[i]);
-                i++;
+             merge.push_back(nums1[i++]);
+                
         }
 
         while(j<n)
         {
-             merge.push_back(nums2[j]);
-                j++;
+             merge.push_back(nums2[j++]);
+                
         }
 
-        for(int i=0 ; i<m+n ; i++)
-        {
-            cout<<merge[i];
-        }
-
-        if((m+n)%2!=0)
-        {
-            int num =(m+n)/2 ;
-            return merge[num];
-        }
-        else
-        {
-            int num1=(m+n)/2;
-            int num2=num1 -1;
+        // if((m+n)%2!=0)
+        // {
+        //     int num =(m+n)/2 ;
+        //     return merge[num];
+        // }
+        // else
+        // {
+        //     int num1=(m+n)/2;
+        //     int num2=num1 -1;
             
-            float result =float(merge[num1] + merge[num2])/2;
-            return result;
+        //     float result =float(merge[num1] + merge[num2])/2;
+        //     return result;
 
-        }
+        // }
+           
 
-        return 0;
+        return (((m+n)%2==1) ? merge[(m+n)/2] : (merge[(m+n)/2]+merge[((m+n)/2)-1])/2.0);
+
+        // return 0;
         
         
     }
