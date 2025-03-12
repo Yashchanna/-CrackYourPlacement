73511fa24886b7b27d44807a130ddcd57709af35
Yashchanna <118649332+Yashchanna@users.noreply.github.com>
2025-03-12 06:41:52 +0530

    Time: 46 ms (63.22%), Space: 54.6 MB (6.2%) - LeetHub

---
+++ 2732-counter/2732-counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        var a=n;
+        n+=1;
+        return a;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
